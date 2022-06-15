96ce8555dc3ba579affa4a14afc81d0d37847ee9
Farith Adnan <ayiedfarith@gmail.com>
2022-06-15 16:37:36 +0800

    Added responsive functionality when app is open in mobile

--- Altr.Frontend/src/app/navigation/navigation.component.ts
+++ Altr.Frontend/src/app/navigation/navigation.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements AfterViewInit {
+  @ViewChild(MatSidenav)
+  sidenav!  : MatSidenav;
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+      this.observer.observe(['(max-width: 800px']).subscribe((res) => {
+        if(res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
   }
 
 }
