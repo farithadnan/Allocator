e8f9c8f40ca06a921d166573db24f895724c55cf
Farith Adnan <ayiedfarith@gmail.com>
2022-06-15 15:17:19 +0800

    Added navigation for the page

--- Altr.Frontend/src/app/app.module.ts
+++ Altr.Frontend/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
+import { RoutingModule } from './routing/routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SidebarContentComponent } from './sidebar-content/sidebar-content.component';
+
+// To make our code much more cleaner
+const Ux_Modules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SidebarContentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    RoutingModule,
+    Ux_Modules
   ],
   providers: [],
   bootstrap: [AppComponent]
