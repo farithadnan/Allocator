9181cfaaa807b853c5ba9695e3de81f795544635
Farith Adnan <ayiedfarith@gmail.com>
2022-07-06 15:35:18 +0800

    Added pipe to seperate first letter uppercase word with spaces
    also, Uppercase first letter of word

---
+++ Altr.Frontend/src/app/foundation/pipes/word-seperator.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { WordSeperatorPipe } from './word-seperator.pipe';
+
+describe('WordSeperatorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new WordSeperatorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
