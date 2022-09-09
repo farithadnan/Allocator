f4ed297195026bfc5fafae45ef7ee7781dfbfec5
Farith <22324133+farithadnan@users.noreply.github.com>
2022-09-09 13:09:07 +0800

    create generic confirmation dialog template to use
    - create a new component for confirmation dialog

---
+++ Altr.Frontend/src/app/foundation/reusable-component/a-ltr-modal/modal-confirm/modal-confirm.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirm',
+  templateUrl: './modal-confirm.component.html',
+  styleUrls: ['./modal-confirm.component.scss']
+})
+export class ModalConfirmComponent implements OnInit {
+  shakeIt = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
